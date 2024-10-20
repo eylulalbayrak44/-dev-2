185314ea90b1b7802468290d908a7f842d16fdac
eylulalbayrak44 <alb.eylul44@gmail.com>
2024-10-20 14:31:23 +0300

    Add files via upload

---
+++ EylülAlbayrak_ödev2.cs
@@ -0,0 +1,111 @@
+﻿//1. soru
+//Console.Write("Bir sayı girin:");
+//int sayı= int.Parse(Console.ReadLine());
+
+//if (sayı % 2 == 0)
+//    Console.WriteLine("Girilen sayı çifttir");
+//else
+//    Console.WriteLine("Girilen sayı tektir");
+//Console.ReadKey();
+
+
+//2. soru
+//int sayı;
+//Console.WriteLine("1 ile 10 arasinda bir sayi giriniz");
+//sayı = int.Parse(Console.ReadLine());
+
+//switch (sayı)
+//{
+//    case 1:
+//        Console.WriteLine("Bir");
+//        break;
+
+//    case 2:
+//        Console.WriteLine("İki");
+//        break;
+
+//    case 3:
+//        Console.WriteLine("Üç");
+//        break;
+
+//    case 4:
+//        Console.WriteLine("Dört");
+//        break;
+
+//    case 5:
+//        Console.WriteLine("Beş");
+//        break;
+
+//    case 6:
+//        Console.WriteLine("Altı");
+//        break;
+
+//    case 7:
+//        Console.WriteLine("Yedi");
+//        break;
+
+//    case 8:
+//        Console.WriteLine("Sekiz");
+//        break;
+
+//    case 9:
+//        Console.WriteLine("Dokuz");
+//        break;
+
+//    case 10:
+//        Console.WriteLine("On");
+//        break;
+//    default:
+//        Console.WriteLine("1 ile 10 arasinda bir sayi giriniz");
+//        break;
+//}
+
+
+//3. soru
+//ka : admin
+//şifre Admin_32453@
+//giriş başarılı 
+//giriş başarısız
+//Console.WriteLine("ka");
+//string ka = Console.ReadLine();
+//Console.WriteLine("şifre");
+//string sifre = Console.ReadLine();
+
+//if (ka == "admin" && sifre == "Admin_32453@")
+//{
+//    Console.WriteLine("Giriş başarılı");
+//}
+//else
+//{
+//    Console.WriteLine("BAŞARISIZ");
+//}
+
+//Console.ReadKey();
+
+
+//4. soru
+//Console.WriteLine("Sıcaklık Değerini Giriniz:");
+//int sıcaklık_degerı = Convert.ToInt32(Console.ReadLine());
+
+
+//if (sıcaklık_degerı > 10 && sıcaklık_degerı < 20)
+//{
+//    Console.WriteLine("normal");
+//}
+//else if (sıcaklık_degerı > 20 && sıcaklık_degerı < 30)
+//{
+//    Console.WriteLine("sıcak");
+//}
+//else if (sıcaklık_degerı > 30 && sıcaklık_degerı < 40)
+//{
+//    Console.WriteLine("çok sıcak");
+//}
+//else
+//{
+//    Console.WriteLine("Aralıkta Olmayan Bir Değer Girdiniz");
+//        }
+
+//Console.ReadKey();
+
+
+
